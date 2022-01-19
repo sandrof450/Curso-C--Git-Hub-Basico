a7cdc57b5a083cc6136a43b818821c468efb0769
Sandro <sandrof450@gmail.com>
2022-01-19 01:02:21 -0300

    Projeto iniciado, mostrando a mensagem olá mundo.

---
+++ Projeto1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Projeto1
+{
+    class Projeto1
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
